505e6a61b5d3ed02d1698733fcfe2d4352006547
KaarJoseph <85071917+KaarJoseph@users.noreply.github.com>
2023-04-19 16:17:37 -0500

    2 files modified

--- estilos.js
+++ estilos.js
@@ -9,3 +9,14 @@ function cambiarEstilo() {
     nuevoEstilo.setAttribute("href", "estilo2.css");
   }
 }
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
